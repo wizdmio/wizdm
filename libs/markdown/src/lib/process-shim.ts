11e0745cc1274d864354771d9db86ae1fbac9ef1
Lucio Francisco <hello@wizdm.io>
2018-10-16 15:11:16 +0000

    Update process-shim.ts

--- libs/markdown/src/lib/process-shim.ts
+++ libs/markdown/src/lib/process-shim.ts
@@ -1,13 +1,10 @@
  /** Implement a minimal 'process' to support node module 'path' used by remark **/
  
- (function() {
-     
-     if(!!window && !window.process) {
-      (window as any).process = {
-        env: { DEBUG: undefined },
-        platform: "",
-        cwd: function(){}
-      }
-    }
-    
- })();
+
+ if(!!window && !window.process) {
+  (window as any).process = {
+    env: { DEBUG: undefined },
+    platform: "",
+    cwd: function(){}
+  }
+}
