e62bd14aada030f971728cf039d7e2dd4dc26ff8
Lucio Francisco <hello@wizdm.io>
2018-10-17 10:38:05 +0000

    Add new file

---
+++ libs/augment/array/shuffle.ts
@@ -0,0 +1,27 @@
+export {};// Ensure this is treated as a module.
+
+declare global {
+  interface Array<T> {
+    /**
+     * Extends Array implementing the Fisher-Yates shuffle algorithm 
+     * (so basically shuffling the elements randomly)
+     */
+    shuffle(): T[];
+  }
+}
+
+Array.prototype.shuffle = function<T>(this: T[]): T[] {
+
+  let result = this;
+
+  for(let i = this.length - 1; i > 0;i--) {
+
+    let rnd = Math.floor(Math.random() * (i + 1));
+        
+        let tmp     = result[i];
+        result[i]   = result[rnd];
+        result[rnd] = tmp;
+    }
+
+    return result;
+}
