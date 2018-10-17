9ac034c28d9976b6b0099f552381da00434aafb6
Lucio Francisco <hello@wizdm.io>
2018-10-17 10:38:40 +0000

    Add new file

---
+++ libs/augment/string/camelize.ts
@@ -0,0 +1,19 @@
+export {};// Ensure this is treated as a module.
+
+declare global {
+
+  interface String {
+  /**
+   * Turns an Hyphen delimited string into a CamelCase version of it
+   * @example "invalid-user".camelize();
+   * @returns "InvalidUser"
+   */
+    camelize(): string;
+  }
+}
+
+String.prototype.camelize = function(this: string): string {
+	return this.replace(/([_.\- ][a-z])/g, $1 => {
+    return $1.toUpperCase().replace(/[_.\- ]/g,'');
+  });
+};
