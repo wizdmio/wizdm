d28f8db48889acba937377c0077c56d8aefc4d64
Lucio Francisco <hello@wizdm.io>
2018-10-17 10:37:35 +0000

    Add new file

---
+++ libs/augment/math/normal.ts
@@ -0,0 +1,19 @@
+export {};// Ensure this is treated as a module.
+
+declare global {
+
+  interface Math {
+  /**
+   * Extends Math with standard normal variate using Box-Muller transform.
+   * (so basically is a random generator with normal distribution)
+   */
+    normal(): number;
+  }
+}
+
+Math.normal = function() {
+
+  var u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
+  var v = 1 - Math.random();
+  return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
