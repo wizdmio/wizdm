3a23fa7d276fe265d3a35f2dd71b149c6227c01c
Lucio Francisco <hello@wizdm.io>
2018-10-17 10:39:50 +0000

    Add new file

---
+++ libs/augment/string/interpolate.ts
@@ -0,0 +1,29 @@
+export {};// Ensure this is treated as a module.
+
+declare global {
+
+    interface String {
+    /** 
+     * Implements a simple interpolation funtion able to replace named {{variables}} into
+     * their corrensponding values. It works at several depth levels {{object.section.value}}
+     * recurring into the object down to the value, when available.
+     * @param context the object containing the name source values to replace interpolation
+     * @returns the same interpolated string 
+     * @example "Try {{this}}".interpolate( { this: "that"} ); returns "Try that"
+     */
+        interpolate(args: any): string;
+    }
+}
+
+String.prototype.interpolate = function(this: string, context: any): string {
+
+// Uses a regular expression to match for {{variableName}} capturing variableName
+// accepting a variable numbers of spaces within the brackets
+  return this.replace(/{{\s*([.\w]+)\s*}}/g, (match, capture) => {
+    return capture
+      .split(".")
+      .reduce( (obj, token) => { 
+        return obj && obj[token];
+      }, context) || match;
+    });
+}
