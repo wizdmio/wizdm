95b5f8ec92b5617a5a1d8a77371c36ebe3bfbd13
Lucio Francisco <hello@wizdm.io>
2018-11-07 15:08:50 +0000

    Update language-resolver.service.ts

--- apps/wizdm/src/app/navigator/resolver/language-resolver.service.ts
+++ apps/wizdm/src/app/navigator/resolver/language-resolver.service.ts
@@ -14,7 +14,7 @@ export class UserLanguageResolver implements ResolveLanguage {
   // Implements a basic language resolver returning the user preferred language
   // captured from the user profile stored in the database. Since this observable
   // pipes from the AuthService this resolver grants the page won't show up before
-  // the user authentication has been checked avoinding user flickering effects
+  // the user authentication has been checked avoinding page flickering effects
   public resolveLanguage(): string | Observable<string> {
 
     return this.user.asObservable()
