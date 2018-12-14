4a2fc4ab2e414d160fd02d6c171ddef68754c024
Lucio <hello@wizdm.io>
2018-12-14 21:54:14 +0100

    got rid of node shim polyfill

--- apps/wizdm/src/polyfills.ts
+++ apps/wizdm/src/polyfills.ts
@@ -71,11 +71,3 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-/***************************************************************************************************
- * CUSTOM NODE SHIMS - since Angular is dropping node shims from version 6
- */
-
- /** Implement a minimal 'process' to support node module 'path' used by remark **/
- // import 'libs/markdown/src/utils/process-shim';
- // Shim is now embedded in markdown module
