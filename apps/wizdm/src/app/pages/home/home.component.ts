2b9a347be0919ba5bb742db1c3f63d88c44a251e
wizdm <hello@wizdm.io>
2020-02-08 21:41:51 +0100

    baseHref in home fixed

--- apps/wizdm/src/app/pages/home/home.component.ts
+++ apps/wizdm/src/app/pages/home/home.component.ts
@@ -6,19 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
   host: { 'class': 'padding-top-toolbar' }
 })
-export class HomeComponent {
-
-  //readonly baseHref$: Observable<string>;
-  //readonly msgs$: Observable<any>;
-
-  constructor() {
-
-    // Gets the localized content pre-fetched during routing resolving
-    //this.msgs$ = content.stream('home');
-
-    // Computes the baseHref making sure svg gradients/filters will work in 
-    // Firefox and Safari although index.html defines <base href="..."> tag 
-    // breaking relative url() resolution (works fine in Chrome, btw) 
-    //this.baseHref$ = this.content.language$.pipe( delay(250) );
-  }
-}
+export class HomeComponent {}
