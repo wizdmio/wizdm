6be11d2f7e6bcf98fc631ceac8c3c8539ec9f291
Michal Partyka <ptychu@gmail.com>
2020-05-11 17:08:56 +0200

    fixup! Fix double click event listener and disable on action component

--- apps/wizdm/src/app/navigator/actionbar/action.component.ts
+++ apps/wizdm/src/app/navigator/actionbar/action.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { MediaObserver } from '@angular/flex-layout';
 import { ThemePalette } from '@angular/material/core';
@@ -26,13 +26,7 @@ export class ActionComponent {
   @Input('disabled') set disableAction(value: boolean) { this.disabled = coerceBooleanProperty(value); }
   public disabled = false;
 
-  /** Emits when clicked */
-  @Output() click = new EventEmitter<MouseEvent>();
-
   public onClick(event: MouseEvent) {
-
-    event.stopPropagation();
-    this.click.emit(event);
 
     // Notifies teh actionbar about the action that has been activated
     this.actionbar.activate(this);
