5dcecacc1b788250789279fb56470c65cca530c1
Lucio <23312256+lasfrancisco@users.noreply.github.com>
2020-05-13 11:46:15 +0200

    Update oauth2-handler.service.ts

--- apps/wizdm/src/app/auth/oauth2-handler/oauth2-handler.service.ts
+++ apps/wizdm/src/app/auth/oauth2-handler/oauth2-handler.service.ts
@@ -1,6 +1,5 @@
 import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
-//import { AuthService } from '@wizdm/connect/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +10,14 @@ export class Oauth2Handler implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot) {
 
+    // Gets the firebase actionlink request data
     const mode = route.queryParamMap.get('mode');
     const code = route.queryParamMap.get('oobCode');
     const api  = route.queryParamMap.get('apiKey');
     const lang = route.queryParamMap.get('lang') || 'en';
     const url  = route.queryParamMap.get('continueUrl') || 'home';
 
-    //...and navigate to the login page with the requested language
+    //...and translates into the proper request for the login module
     return this.router.createUrlTree([lang, 'login'], { 
       queryParams: { mode, code, url },
       //replaceUrl: true
