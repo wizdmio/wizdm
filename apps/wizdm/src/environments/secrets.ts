ba9b5b7408cdb8cd8129355d47503d4be530ec98
Lucio <23312256+lasfrancisco@users.noreply.github.com>
2020-05-19 15:15:45 +0200

    Create secrets.ts

---
+++ apps/wizdm/src/environments/secrets.ts
@@ -0,0 +1,22 @@
+import { ConnectConfig } from '@wizdm/connect';
+import { DoorbellConfig } from '@wizdm/doorbell';
+import { GtagConfig } from '@wizdm/gtag';
+
+export const firebase: ConnectConfig = {
+  apiKey: "<< YOUR FIREBASE API KEY HERE >>",
+  authDomain: "<< your firebase auth domain here >>",
+  databaseURL: "<< your firebase database url here >>",
+  projectId: "<< your firebase project id here >>",
+  storageBucket: "<< your firebase storage bucked here >>",
+  messagingSenderId: "<< YOUR FIREBASE MESSAGE SENDER ID HERE >>"
+};
+
+export const doorbell: DoorbellConfig = {
+  url: "https://doorbell.io/api/applications",
+  id: "<< your doorbell id here >>",
+  appKey: "<< your doorbell app key here >>"  
+};
+
+export const gtag: GtagConfig = {
+  targetId: '<< YOUR GA MEASURE ID HERE >>'
+};
