8e14ce545e61ab08ee3b6f379ecf3ea6997e428b
Lucio <23312256+lasfrancisco@users.noreply.github.com>
2020-05-19 14:58:57 +0200

    fake secrets.ts

---
+++ apps/wizdm/src/environments/secrets.ts
@@ -0,0 +1,18 @@
+export const firebase: ConnectConfig = {
+  apiKey: "<< YOUR FIREBASE API KEY HERE >>",
+  authDomain: "<< your firebase auth domain here >>",
+  databaseURL: "<< your firebase database url here >>",
+  projectId: "<< your firebase project id here >>",
+  storageBucket: "<< your firebase storage bucked here >>",
+  messagingSenderId: "<< YOUR FIREBASE MESSAGE SENDER ID HERE >>"
+};
+
+export const doorbell: DoorbellConfig = {
+  url: "https://doorbell.io/api/applications",
+  id: "<< your doorbell id here >>",
+  appKey: "<< your doorbell app key here >>"  
+};
+
+export const gtag: GtagConfig = {
+  targetId: '<< YOUR GA MEASURE ID HERE >>'
+};
