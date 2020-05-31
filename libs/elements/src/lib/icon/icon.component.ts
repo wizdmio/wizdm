d53382a18739065508f4820dbf65573200b22302
wizdm <hello@wizdm.io>
2020-05-31 16:27:43 +0200

    extended material icons for wm-icon

--- libs/elements/src/lib/icon/icon.component.ts
+++ libs/elements/src/lib/icon/icon.component.ts
@@ -15,13 +15,21 @@ export class IconComponent {
   public fontIcon: string;
   public matIcon = "code";
 
+  /** Inlines the icon automatically sizing it to match the font size */
   @Input('inline') set inlining(value: boolean) { this.inline = coerceBooleanProperty(value); }
   public inline: boolean = false;
 
+  /** Enables extended material icons */
+  @Input('extended') set extending(value: boolean) { this.extended = coerceBooleanProperty(value); }
+  public extended: boolean;
+
+  /** Theme color palette */
   @Input() color: ThemePalette;
 
+  /** Icon name */
   @Input('icon') set setIcon(descriptor: string) {
    
+    // Splits the descriptor between set:name
     const segments = descriptor ? descriptor.split(':') : [];
     
     if(segments.length > 1) {
@@ -32,8 +40,10 @@ export class IconComponent {
     }
     else {
       
-      this.fontSet = this.fontIcon = undefined;
       this.matIcon = segments[0];
+      // Switches to 'material-icons-extended' on request
+      this.fontSet = this.extended ? 'material-icons-extended': undefined;
+      this.fontIcon = undefined;
     }
   }
 }
