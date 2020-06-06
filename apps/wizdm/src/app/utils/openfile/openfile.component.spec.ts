4b626884da43892457d62105e2ea404448eb22b7
Paul Oloyede <ayopaulofficial@gmail.com>
2020-06-06 23:33:48 +0100

    move openfile to utils

---
+++ apps/wizdm/src/app/utils/openfile/openfile.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OpenFileComponent } from './openfile.component';
+
+describe('OpenFileComponent', () => {
+  let component: OpenFileComponent;
+  let fixture: ComponentFixture<OpenFileComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [OpenFileComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OpenFileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
