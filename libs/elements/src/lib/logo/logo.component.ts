1456e2c09ad2741019c49cb34a678e88a35d865f
wizdm <hello@wizdm.io>
2020-06-10 19:04:02 +0200

    new logo with animation

--- libs/elements/src/lib/logo/logo.component.ts
+++ libs/elements/src/lib/logo/logo.component.ts
@@ -1,23 +1,34 @@
-import { Component, Input, HostBinding, ViewEncapsulation } from '@angular/core';
+import { Component, Input, HostBinding, HostListener, ViewEncapsulation } from '@angular/core';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { ThemePalette } from '@angular/material/core'
 
 @Component({
   selector: 'wm-logo',
   templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss'],
-  host: { 'class': 'wm-logo' },
+  styleUrls: ['./logo.component.scss'], 
+  host: { 
+    'class': 'wm-logo',
+    '[attr.color]': 'color',
+    '[attr.inline]': 'inline',
+    '[class.animate]': 'animate'
+  },
   encapsulation: ViewEncapsulation.None
 })
 export class LogoComponent {
 
-  // Color customization 
-  @HostBinding('attr.color')
+  public animate: boolean
+  public inline: boolean;
+
+  /** Theme color */ 
   @Input() color: ThemePalette = 'primary';
 
-  /** Inlines the logo so the height automatically adapts to the font size */
-  @Input() inline: boolean;
-  @HostBinding('attr.inline') get inlineAttr() { 
-    return coerceBooleanProperty(this.inline);
+  /** Automatically adjust the size to the font height */
+  @Input('inline') set inlining(inline: boolean) {
+    this.inline = coerceBooleanProperty(inline);
+  }
+
+  /** Runs the spinning animation */
+  @Input('animate') set runAnimation(animate: boolean) {
+    this.animate = coerceBooleanProperty(animate);
   }
 }
