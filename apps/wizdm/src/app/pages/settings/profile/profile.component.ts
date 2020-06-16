af9a08bcef7f80fe630db3891525ccf5ed0fffd2
wizdm <hello@wizdm.io>
2020-06-16 22:50:57 +0200

    profile fixes

--- apps/wizdm/src/app/pages/settings/profile/profile.component.ts
+++ apps/wizdm/src/app/pages/settings/profile/profile.component.ts
@@ -1,5 +1,6 @@
-import { UserProfile } from 'app/navigator/providers/user-profile';
-import { Component } from '@angular/core';
+import { UserProfile, UserData } from 'app/navigator/providers/user-profile';
+import { UserFormComponent } from './user-form/user-form.component';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'wm-profile',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
-  constructor(readonly user: UserProfile) {}  
+  @ViewChild(UserFormComponent) form: UserFormComponent;
+
+  constructor(private user: UserProfile) {} 
+
+  /* The current user profile data */
+  get profile(): UserData { return this.user.data; }
+
+  /** Updates the profile data */
+  public updateProfile(data: UserData) {
+
+    return this.user.update(data)
+      .then( () => this.form.markAsPristine() );
+  }
+
+  /** Updates the profile photo */
+  public updatePhoto(photo: string) {
+    return this.user.update({ photo });
+  }
 }
