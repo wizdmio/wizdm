719cf3f35a704698780a87272532121d80c8468b
wizdm <hello@wizdm.io>
2020-06-19 16:46:35 +0200

    Button changer with media query support

--- libs/elements/src/lib/button/button-changer.module.ts
+++ libs/elements/src/lib/button/button-changer.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ButtonTypeChanger } from './button-changer.directive';
+import { ButtonMediaChanger } from './button-media-changer.directive';
 
 @NgModule({
   imports: [],
-  declarations: [ ButtonTypeChanger ],
-  exports: [ ButtonTypeChanger ]
+  declarations: [ ButtonTypeChanger, ButtonMediaChanger ],
+  exports: [ ButtonTypeChanger, ButtonMediaChanger ]
 })
 export class ButtonChangerModule { }
