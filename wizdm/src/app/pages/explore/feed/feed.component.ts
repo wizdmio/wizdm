1757792d64efd02cb2e5307a5a498c44d1572ac7
wizdm <hello@wizdm.io>
2021-04-25 22:10:18 +0200

    feed unread count fixes

--- wizdm/src/app/pages/explore/feed/feed.component.ts
+++ wizdm/src/app/pages/explore/feed/feed.component.ts
@@ -1,14 +1,12 @@
-import { onSnapshot, where, orderBy, limit, endBefore, docs, snap, page, data } from '@wizdm/connect/database/collection/operators';
+import { filter, take, map, tap, expand, startWith, distinctUntilChanged, shareReplay, switchMap, takeWhile } from 'rxjs/operators';
+import { onSnapshot, where, orderBy, endBefore, docs, page } from '@wizdm/connect/database/collection/operators';
 import { DatabaseGroup, QueryDocumentSnapshot } from '@wizdm/connect/database/collection';
-import { filter, take, skip, map, tap, expand, startWith, distinctUntilChanged, shareReplay, switchMap } from 'rxjs/operators';
 import { DatabaseService } from '@wizdm/connect/database';
-import { UserProfile, UserData } from 'app/utils/user';
+import { Observable, BehaviorSubject, of } from 'rxjs';
 import { ScrollObservable } from 'app/utils/scrolling';
 import { MediaObserver } from '@angular/flex-layout';
-import { EmojiUtils } from '@wizdm/emoji/utils';
 import { PostData } from './post/post.component';
 import { Component } from '@angular/core';
-import { Observable, BehaviorSubject, of } from 'rxjs';
 
 @Component({
   selector: 'wm-feed',
@@ -71,28 +69,27 @@ export class FeedComponent extends DatabaseGroup<PostData> {
     // Tracks whenever new posts arrived since last refresh
     this.unread$ = this.feed$.pipe( 
       // Filters for changes based on the latest post to avoid unecessary calls to the database
-      distinctUntilChanged((x,y) => x[0] === y[0]),       
-      // Extracts the latest post only
-      map( posts => posts.splice(0, 1)),          
-      // Stream for new comers
-      expand(posts => this.pipe(
-        // Query for more posts 
-        where('tags', 'array-contains', 'public'), orderBy('created', 'desc'),
-        // Streams the latest document snapshot
-        endBefore(posts[0]), onSnapshot(this.db.zone),
-        // Filters out not only the empty emissions but also the local ones (still having pending writes).
-        filter(posts => posts.size > 0 && !posts.metadata.hasPendingWrites),
-        // Takes only one new emission at a time and append the result to the previous posts to minimize the traffic
-        take(1), docs(), map(newcomers => newcomers.concat(posts))
+      distinctUntilChanged((x,y) => x[0] === y[0]),      
+      // Switch to an inner observable with the latest post only
+      switchMap(posts => of(posts.splice(0, 1)).pipe(
+        // Stream for new comers
+        expand(posts => this.pipe(
+          // Query for more posts 
+          where('tags', 'array-contains', 'public'), orderBy('created', 'desc'),
+          // Streams the latest document snapshot
+          endBefore(posts[0]), onSnapshot(this.db.zone),
+          // Filters out not only the empty emissions but also the local ones (still having pending writes).
+          filter(posts => posts.size > 0 && !posts.metadata.hasPendingWrites),
+          // Takes only one new emission at a time and append the result to the previous posts to minimize the traffic
+          take(1), docs(), map(newcomers => newcomers.concat(posts)),
+        )),
+        // Counts the new comers only (subtracting the single original latest post from paging)
+        map(posts => posts.length - 1), 
+        // Terminates the inner observable whenever the counter reaches 11 (10+)
+        takeWhile( count => count <= 11 )
       )),
-      // Counts the new comers only (subtracting the single original latest post from paging)
-      map(posts => posts.length - 1),
       // Maps the number of unread posts into the corresponfing label
-      map( count => count <= 0 ? '' : (count > 10 ? '10+' : count.toString()) ), 
-      // Filters for changes only
-      distinctUntilChanged(),
-      // Starts with an empty label
-      startWith('')
+      map( count => count <= 0 ? '' : (count > 10 ? '10+' : count.toString()) )
     );
   }
 }
